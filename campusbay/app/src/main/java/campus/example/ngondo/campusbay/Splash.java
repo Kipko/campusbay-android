d0841a5ed642c9cb5f2bcfd0e9930d898746cd36
Cornellius Ngondo <devngondo@gmail.com>
2015-05-07 19:09:15 +0000

    Modified login screen

--- campusbay/app/src/main/java/campus/example/ngondo/campusbay/Splash.java
+++ campusbay/app/src/main/java/campus/example/ngondo/campusbay/Splash.java
@@ -1,34 +1,32 @@
 package campus.example.ngondo.campusbay;
 
+import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.Window;
 import android.view.WindowManager;
 import android.os.Handler;
-import com.twitter.sdk.android.Twitter;
-import com.twitter.sdk.android.core.TwitterAuthConfig;
-import io.fabric.sdk.android.Fabric;
+//import com.twitter.sdk.android.Twitter;
+//import com.twitter.sdk.android.core.TwitterAuthConfig;
+//import io.fabric.sdk.android.Fabric;
 
 
-public class Splash extends ActionBarActivity {
+public class Splash extends Activity {
+    
 
     // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
-    private static final String TWITTER_KEY = "aAK88Rb2zfgAgjavMpZRWVR7t";
-    private static final String TWITTER_SECRET = "bY1oQGOmljbX2C3E6V2miVPVhQR0cEbW4ONz4obBnkW5umQVp9";
+//    private static final String TWITTER_KEY = "aAK88Rb2zfgAgjavMpZRWVR7t";
+//    private static final String TWITTER_SECRET = "bY1oQGOmljbX2C3E6V2miVPVhQR0cEbW4ONz4obBnkW5umQVp9";
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
-        Fabric.with(this, new Twitter(authConfig));
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
+//        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+//        Fabric.with(this, new Twitter(authConfig));
         setContentView(R.layout.activity_splash);
 
         new Handler().postDelayed(new Runnable() {
@@ -36,29 +34,14 @@ public class Splash extends ActionBarActivity {
             public void run() {
                 Intent i = new Intent(Splash.this,Home.class);
                 startActivity(i);
+                
+                this.finish();
+            }
+            private void finish() {
+
             }
         },3000);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_splash, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
