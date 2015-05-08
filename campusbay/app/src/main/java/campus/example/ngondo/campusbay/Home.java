6ab8a40364a9f8fc0a729442ef07db0bd81f17e8
Cornellius Ngondo <devngondo@gmail.com>
2015-05-08 08:50:13 +0000

    Revert "Layout"

--- campusbay/app/src/main/java/campus/example/ngondo/campusbay/Home.java
+++ campusbay/app/src/main/java/campus/example/ngondo/campusbay/Home.java
@@ -1,28 +1,50 @@
 package campus.example.ngondo.campusbay;
 
-
-
-
+import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
 import it.neokree.materialnavigationdrawer.MaterialNavigationDrawer;
-
-
-
 
 
 public class Home extends MaterialNavigationDrawer {
 
     @Override
-    public void init(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
         this.setDrawerHeaderImage(R.drawable.header1);
 
         this.addSection(newSection("Main", new Main()));
         this.addSection(newSection("Furniture", new Furniture()));
-        this.addSection(newSection("Main", new Electronics()));
-        this.addSection(newSection("Furniture", new Services()));
-        this.addSection(newSection("Main", new Fashion()));
-        this.addSection(newSection("Furniture", new Foods()));
-        this.addSection(newSection("Main", new Others()));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void init(Bundle bundle) {
 
     }
 }
