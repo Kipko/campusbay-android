2e259c4045e4135a39e17639984e9fa8484396f6
Felix Too <felix.too@caytree.net>
2015-05-08 12:00:39 +0300

    Database model created..

---
+++ campusbay/app/src/main/java/campus/example/ngondo/campusbay/Products.java
@@ -0,0 +1,18 @@
+package campus.example.ngondo.campusbay;
+
+import android.provider.ContactsContract;
+
+import java.math.BigInteger;
+
+/**
+ * Created by felix_000 on 5/8/2015.
+ */
+public class Products {
+    public String Id;
+    public String Title;
+    public String Description;
+    public float Price;
+    public int Category;
+    public String Email;
+    public BigInteger Phone_Number;
+}
